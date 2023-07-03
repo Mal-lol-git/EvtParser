50151c8e236add93ce68e0c97cb9c9d7964c325d
mal-lol-git <goddder125@gmail.com>
2023-07-03 16:12:00 +0900

    update_20230703_4

--- setting.py
+++ setting.py
@@ -10,7 +10,7 @@ _PATH = input("EVTLog Folder Path : ")
 _SRC = input("EVTLog Start Date ex)9999-01-01 : ")
 _DST = input("EVTLog Last Date ex)9999-12-30 : ")
 
-_CSV_PATH = input("Save File Folder Path : ")
+_CSV_PATH = input("File Save Path : ")
 
 #today = datetime.datetime.now().date()
 #day_ago = today - datetime.timedelta(days=1)
