a51f6ea6088bcb264d31859c8331b3c5368405af
Mal-lol-git <goddder125@gmail.com>
2023-10-05 10:37:50 +0900

    update_20231005

--- EvtScan/EvtParser_class.py
+++ EvtScan/EvtParser_class.py
@@ -2,6 +2,8 @@
 import os
 import sys
 import csv
+import re
+import sqlite3
 import win32evtlogutil
 import win32evtlog as winevt
 
@@ -15,7 +17,8 @@ class EvtParser():
 		self._PATHTYPE = None
 		self.SRC = None
 		self.DST = None
-		self._CSV_PATH = None
+		self._SAVE_PATH = None
+		self._SAVE_TYPE = None
 		self.EVENT_FILE = []
 		self.RESULT = []
 		self.EVENTID = None
@@ -26,8 +29,9 @@ class EvtParser():
 		self._PATH, self._PATHTYPE = self._EventLogPath()
 		self.SRC, self.DST = self._EventLogDate()
 		self.Select_EventID = self._EventID()
-		self._CSV_PATH = self._EventLogSavePath()
-		self._Scan(self._PATH, self._PATHTYPE, self.SRC, self.DST, self._CSV_PATH)
+		self._SAVE_PATH = self._EventLogSavePath()
+		self._SAVE_TYPE = self._EventLogSaveType()
+		self._Scan(self._PATH, self._PATHTYPE, self.SRC, self.DST, self._SAVE_PATH, self._SAVE_TYPE)
 
 
 	def _EventLogPath(self):
@@ -87,13 +91,30 @@ class EvtParser():
 				self.EVENTID = input("[EventID]\n EventID ex) 7045, 7046\n >> ").replace(' ','').split(',')
 				return Select_EventID
 		except Exception as e:
-			print(e)			
+			print(e)	
 
 	def _EventLogSavePath(self):
 		try:
 			return input("[EventLog Save Path]\n File Save Path : ")
 		except Exception as e:
 			print(e)
+
+	def _EventLogSaveType(self):
+		while True:
+			try:
+				Save_Type = int(input("\n[EventLog Save Type]\n 1.CSV\n 2.DB\n 3.Exit\n >> "))
+				if Save_Type == 1 or Save_Type == 2:
+					return Save_Type
+				if Save_Type == 3:
+					sys.exit(0)
+				else:
+					print("\n[Error]\n Please Input Number '1-3'\n")
+					os.system('pause')
+					os.system('cls')
+			except Exception as e:
+				print("\n[Error]\n Please Input Number '1-3'\n")
+				os.system('pause')
+				os.system('cls')
 
 	def _LocalEvtLogHandle(self, logtype):
 		try:
@@ -135,7 +156,7 @@ class EvtParser():
 		except Exception as e:
 			print(e)
 
-	def _Scan(self, path, pathtype, src, dst, csv_path):
+	def _Scan(self, path, pathtype, src, dst, save_path, save_type):
 		count=0
 		self._FileList(path)
 		for filename in self.EVENT_FILE:
@@ -155,9 +176,14 @@ class EvtParser():
 					for evt in events:
 						if str(evt.TimeGenerated)[:10]:
 							if count == 30000:
-								self.EvtCsv(self.RESULT, _LOG_TYPE, csv_path)
-								self.RESULT.clear()
-								count = 0
+								if save_type == 1:
+									self.EvtCsv(self.RESULT, _LOG_TYPE, save_path)
+									self.RESULT.clear()
+									count = 0
+								if save_type == 2:	
+									self.EvtDB(self.RESULT, _LOG_TYPE, save_path)
+									self.RESULT.clear()
+									count = 0
 							if dst >= evt.TimeGenerated >= src:
 								if self.Select_EventID:
 									if str(evt.EventID & 0x1FFFFFFF) in self.EVENTID:
@@ -170,8 +196,12 @@ class EvtParser():
 							break
 				else:
 					if self.RESULT:
-						self.EvtCsv(self.RESULT, _LOG_TYPE, csv_path)
-						self.RESULT.clear()
+						if save_type == 1:
+							self.EvtCsv(self.RESULT, _LOG_TYPE, save_path)
+							self.RESULT.clear()
+						if save_type == 2:	
+							self.EvtDB(self.RESULT, _LOG_TYPE, save_path)
+							self.RESULT.clear()
 					print('Number of Logs Parsing : %d' % count)
 					print('*'*54)
 					count = 0
@@ -179,11 +209,16 @@ class EvtParser():
 
 	def _Result(self, evt, filename):
 		TimeGenerated = evt.TimeGenerated
+		TimeWritten = evt.TimeWritten
 		EventID = evt.EventID & 0x1FFFFFFF
 		EventLog = filename[:-5]
 		SourceName = evt.SourceName
+		ComputerName = evt.ComputerName
 		try:
 			description = win32evtlogutil.SafeFormatMessage(evt, filename[:-5])
+			des_result = re.search('following insertion string\(s\)\:\'(.*)\'\.>', description)
+			if des_result != None:
+				description = des_result.group(1)
 		except Exception:
 			description = ''
 		if evt.StringInserts:
@@ -191,7 +226,7 @@ class EvtParser():
 		else:
 			description2 = ''
 		description2 = '||'.join(description2)
-		return TimeGenerated, EventID, EventLog, SourceName, description, description2
+		return str(TimeGenerated), str(TimeWritten), EventID, EventLog, SourceName, ComputerName, description, description2
 
 	def EvtCsv(self, result, logtype, path):
 		try:
@@ -208,3 +243,22 @@ class EvtParser():
 			print('CSV FileName : ', SAVE_FILENAME)
 		except Exception as e:
 			print(e)
+
+	def EvtDB(self, result, logtype, path):
+		try:
+			con = sqlite3.connect(os.path.join(path, 'EVTDB.db'))
+			cur = con.cursor()
+			#tablename = re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z]", '', logtype)
+			tablename = re.sub(r"[^0-35-9a-zA-Zㄱ-ㅣ가-힣]", '', logtype)
+			table_check_query = "select count(*) from sqlite_master where name='" + tablename + "'"
+			cur.execute(table_check_query)
+			if not bool(cur.fetchone()[0]):
+				query = "CREATE TABLE " + tablename + " (TimeGenerated text, TimeWritten text, EventID int, EventLog text, SourceName text, ComputerName text, Orgin_Description text, Description text)"
+				cur.execute(query)
+			#print(result)
+			cur.executemany('INSERT INTO ' + tablename + ' VALUES (?,?,?,?,?,?,?,?)', result)
+			con.commit()
+			con.close()
+		except Exception as e:
+			print(e)
+
